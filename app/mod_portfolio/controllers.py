3bd243a944f7a9d7e6745cf63d468e121b061966
Nithin Kumar <nithin.kumar@practo.com>
2016-12-25 23:22:50 +0530

    master

--- app/mod_portfolio/controllers.py
+++ app/mod_portfolio/controllers.py
@@ -1,7 +1,7 @@
 """Authentication Module Controllers."""
 from flask import Blueprint, render_template
 
-mod_portfolio = Blueprint('user', __name__, url_prefix='/portfolio')
+mod_portfolio = Blueprint('user', __name__, url_prefix='/')
 
 
 # Signup method
